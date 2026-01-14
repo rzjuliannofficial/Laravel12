b9d6deb34fa38401ef279db466b25c6b285b47b7
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-01-14 15:52:32 +0700

    `Removed navigasi.blade.php file` ,and modify

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-</div>
+@props(['active' -> false])
+<a {{ $attributes }} 
+    class="{{ $active ? ' bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} 
+    rounded-md px-3 py-2 text-sm fo nt-medium"
+    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
