91ffc6708426de0d3313d1cef614f7b455a78df6
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-01-16 16:31:25 +0700

    make blogs into single blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,15 +1,16 @@
 <x-layout>
-    
+
     <x-slot:title> {{ $title }}</x-slot:title>
 
-    <h1 class="text-lg">Welcome to My Blog</h1>
-    <p>this time is {{ time() }}</p>
-    <article class="py-4">
-        <h3>Judul 1</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.</p>
+    
+    <article class="py-4 max-w-screen-md border-b border-gray-300">
+        <h3 class="text-3xl font-bold tracking-tight">{{ $blog['title'] }}</h3>
+        <div class="text-base text-gray-500">
+            <a href="">{{ $blog['author'] }}</a> | {{ $blog['date'] }}
+        </div>
+        <p class="my-4 font-light">{{ $blog['body'] }}</p>
+        <a href="/blogs" class="font-medium text-blue-500 hover:underline">&laquo; Back to Blogs </a>
     </article>
-    <article>
-        <h3>Judul 2</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.</p>
-    </article>
+    
+    
 </x-layout>
