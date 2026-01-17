d1df4e5fc172f3fc36c7229c92039ee642bdd144
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-01-17 15:01:57 +0700

    add blog.php for models, n make web.php read data for models blog.php

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,9 @@
 <?php
 
+use App\Models\Blog;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-class Blog {
-    public static function all(){
-        return [
-            [
-                'id'=> 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Nabhan Rizqi Julian Saputro', 
-                'date' => '14 Juni 2026',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
-            ],
-            [
-                'id'=> 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Nabhan Rizqi Julian Saputro', 
-                'date' => '15 Juni 2026',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
-            ],
-            [
-                'id'=> 3,
-                'slug' => 'judul-artikel-3',
-                'title' => 'Judul Artikel 3',
-                'author' => 'Nabhan Rizqi Julian Saputro', 
-                'date' => '14 Januari 2026',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
-            ]
-        ];
-    }
-}
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page'],['name' => 'Julian']);
