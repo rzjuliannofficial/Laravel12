054f20eab96aafb6cc2d22ddb9e429d25aa34f58
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-01-17 14:56:36 +0700

    make class blog for raise the array for blog

--- routes/web.php
+++ routes/web.php
@@ -3,53 +3,9 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home', ['title' => 'Home Page'],['name' => 'Julian']);
-});
-Route::get('/welcome', function () {
-    return view('welcome', ['title' => 'Welcome']);
-});
-
-Route::get('/about', function () {
-    return view('about', ['title' => 'About']);
-});
-
-Route::get('/blogs', function () {
-    return view('blogs', [
-        'title' => 'Blogs',
-        'blogs' => [
-            [
-                'id'=> 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Nabhan Rizqi Julian Saputro', 
-                'date' => '14 Juni 2026',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
-            ],
-            [
-                'id'=> 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Nabhan Rizqi Julian Saputro', 
-                'date' => '15 Juni 2026',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
-            ],
-            [
-                'id'=> 3,
-                'slug' => 'judul-artikel-3',
-                'title' => 'Judul Artikel 3',
-                'author' => 'Nabhan Rizqi Julian Saputro', 
-                'date' => '14 Januari 2026',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
-            ]
-        ]
-    ]);
-});
-
-// halaman single blog
-//Route::get('/blogs/{id}', function($id){ ini menggunakan $id
-Route::get('/blogs/{slug}', function($slug){
-    $blogs = [
+class Blog {
+    public static function all(){
+        return [
             [
                 'id'=> 1,
                 'slug' => 'judul-artikel-1',
@@ -75,8 +31,29 @@ Route::get('/blogs/{slug}', function($slug){
                 'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nemo qui sunt fugiat temporibus, quas sequi praesentium, vel adipisci incidunt nihil esse maiores explicabo aut! Cum incidunt tenetur similique laudantium.'
             ]
         ];
+    }
+}
 
-    $blog = Arr::first($blogs, function($blog) use ($slug){
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page'],['name' => 'Julian']);
+});
+Route::get('/welcome', function () {
+    return view('welcome', ['title' => 'Welcome']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/blogs', function () {
+    return view('blogs', ['title' => 'Blogs', 'blogs' => Blog::all()]);
+});
+
+// halaman single blog
+//Route::get('/blogs/{id}', function($id){ ini menggunakan $id
+Route::get('/blogs/{slug}', function($slug){
+
+    $blog = Arr::first(Blog::all(), function($blog) use ($slug){
         return $blog['slug'] == $slug;
     });
 
